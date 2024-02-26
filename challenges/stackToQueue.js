55a1f4cf1b535a82574fd59b7e047843a4be2024
GianMarcoAlagna <marcoalagna1@gmail.com>
2024-02-25 23:17:13 -0600

    Added comments to explain problem

--- challenges/stackToQueue.js
+++ challenges/stackToQueue.js
@@ -1,24 +1,20 @@
-//* implement a stack using queues as the underlying data structure
+// implement a stack using queues as the underlying data structure
+// for example, let stack = new stackToQueue()
+// stackToQueue.push(1) => undefined
+// stackToQueue.pop() => 1
+// stackToQueue.push(1) => undefined
+// stackToQueue.push(2) => undefined
+// stackToQueue.pop() => 2
+// stackToQueue.pop() => 1
+// stackToQueue.pop() => undefined
+// REMEMBER: That a stack uses Last-In First-Out(LIFO) order, while queues maintain a First-in First-Out(FIFO) order
+// https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+// https://en.wikipedia.org/wiki/Queue_(abstract_data_type)
+
+// How can you intertwine the functionality of two queues such that they maintain a stack order?
+
 class stackToQueue {
-  constructor() {
-    this.top = [];
-    this.bottom = [];
-  }
-
-  push(value) {
-    this.bottom.push(value);
-  }
-
-  pop() {
-    while (this.bottom.length > 1) {
-      this.top.push(this.bottom.shift());
-    }
-    const result = this.bottom.shift();
-    while (this.top.length > 0) {
-      this.bottom.push(this.top.shift());
-    }
-    return result;
-  }
+  constructor() {}
 }
 
 module.exports = { stackToQueue };
