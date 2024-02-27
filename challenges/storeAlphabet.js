263b9cd8fd6f31dbb8cbccb6ebb29e108382318b
Gian-Marco Alagna <43712053+GianMarcoAlagna@users.noreply.github.com>
2024-02-26 22:15:08 -0600

    Remove code from storeAlphabet

--- challenges/storeAlphabet.js
+++ challenges/storeAlphabet.js
@@ -10,24 +10,6 @@
 // Characters not in the alphabet should be deprioritized
 
 function storeAlphabet(str1, str2, alphabet) {
-  // Create a dictionary to map characters to their positions in the alphabet
-  const alphaDict = {};
-  for (let i = 0; i < alphabet.length; i++) {
-    alphaDict[alphabet[i]] = i;
-  }
-
-  return ([str1, str2].sort((a, b) => {
-    const len = Math.min(a.length, b.length);
-    for (let i = 0; i < len; i++) {
-      const aCode = alphaDict[a[i]];
-      const bCode = alphaDict[b[i]];
-      if (aCode > bCode) return 1;
-      if (aCode < bCode) return -1;
-    }
-
-    if (len === a.length) return -1;
-    else return 1;
-  }))[0];
 }
 
 // HINT: Think about how you check what order the characters come in? Could indexing help?
